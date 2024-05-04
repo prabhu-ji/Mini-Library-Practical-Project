b3a5a6b7c1aecf2634750243b8d2db2bcdccca9b
Prabhu Pathak <113412091+prabhu-ji@users.noreply.github.com>
2024-05-05 03:32:45 +0530

    About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Blog() {
+function About() {
   return (
     <div>
-      <h2>Library System Blog</h2>
-      <p>Stay tuned for updates, tips, and tricks related to the library system. We'll be sharing useful information to enhance your experience with the system.</p>
+      <h2>About the Library System</h2>
+      <p>This library system is designed to manage books efficiently. It allows users to add new books to the system, view a list of added books, and explore various functionalities.</p>
     </div>
   );
 }
 
-export default Blog;
+export default About;
