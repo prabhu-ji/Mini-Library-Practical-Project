3741d7a9c4f081b59273e9746a8b63e9f33a946f
Prabhu Pathak <113412091+prabhu-ji@users.noreply.github.com>
2024-05-05 03:28:17 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css'; 
+import './App.css';
 import Home from './components/Home';
 import About from './components/About';
 import Blog from './components/Blog';
@@ -9,9 +9,40 @@ import BookList from './components/BookList';
 function App() {
   const [page, setPage] = useState('home');
   const [books, setBooks] = useState([]);
+  const [filterTags, setFilterTags] = useState([]);
 
   const addBook = (newBook) => {
-    setBooks([...books, newBook]);
+    setBooks([...books, { ...newBook, count: newBook.quantity }]);
+  };
+
+  const handleIssue = (index) => {
+    const updatedBooks = [...books];
+    if (updatedBooks[index].count > 0) {
+      updatedBooks[index].count -= 1;
+      setBooks(updatedBooks);
+    }
+  };
+
+  const handleReturn = (index) => {
+    const updatedBooks = [...books];
+    if (updatedBooks[index].count < updatedBooks[index].quantity) {
+      updatedBooks[index].count += 1;
+      setBooks(updatedBooks);
+    }
+  };
+
+  const handleDelete = (index) => {
+    const updatedBooks = [...books];
+    updatedBooks.splice(index, 1);
+    setBooks(updatedBooks);
+  };
+
+  const handleFilterChange = (tags) => {
+    if (filterTags.includes(tags)) {
+      setFilterTags(filterTags.filter((tag) => tag !== tags));
+    } else {
+      setFilterTags([...filterTags, tags]);
+    }
   };
 
   const renderPage = () => {
@@ -25,7 +56,15 @@ function App() {
       case 'add-book':
         return <AddBook addBook={addBook} />;
       case 'book-list':
-        return <BookList books={books} />;
+        return (
+          <BookList
+            books={books}
+            handleIssue={handleIssue}
+            handleReturn={handleReturn}
+            handleDelete={handleDelete}
+            handleFilterChange={handleFilterChange}
+          />
+        );
       default:
         return <Home />;
     }
@@ -53,9 +92,7 @@ function App() {
         </ul>
       </nav>
 
-      <div className="content">
-        {renderPage()}
-      </div>
+      <div className="content">{renderPage()}</div>
     </div>
   );
 }
